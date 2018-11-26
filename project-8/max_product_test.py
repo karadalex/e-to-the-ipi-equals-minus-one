d9154e0ec268906d66cfaf592e69128f524b317b
Alexios Karadimos <karadalex@gmail.com>
2018-11-26 23:14:43 +0200

    added second test case (non-zero digits)

--- project-8/max_product_test.py
+++ project-8/max_product_test.py
@@ -1,5 +1,6 @@
 import unittest
-from max_product import findMaxProduct1 as findMaxProduct
+# from max_product import findMaxProduct2 as findMaxProduct
+from max_product import findMaxProduct2 as findMaxProduct
 
 class TestProducts(unittest.TestCase):
 
@@ -12,6 +13,18 @@ class TestProducts(unittest.TestCase):
         res = findMaxProduct(numberString, 4)
 
         self.assertEquals(res, 5832)
+
+    # Test case with non-zero digits
+    def test_case2(self):
+        numberFile = open("number2.txt", "r")
+        numberString = ""
+        for line in numberFile:
+            numberString += line
+        numberString = numberString.replace("\n", "")
+        res = findMaxProduct(numberString, 4)
+
+        # max product is 9 x 9 x 9 x 9 = 6561
+        self.assertEquals(res, 6561)
 
 if __name__ == '__main__':
     unittest.main()
