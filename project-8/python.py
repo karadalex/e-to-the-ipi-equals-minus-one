7dc4873857d610b0f060787d8c9d6554fa628019
Alexios Karadimos <karadalex@gmail.com>
2018-11-26 19:58:14 +0200

    made code moremodular, by encapsulating main logic code inside a function (useful for more convenient testing)

--- project-8/python.py
+++ project-8/python.py
@@ -1,23 +1,29 @@
 import sys
 
+"""
+num: digits in string form
+r:   lenght/range of each product group
+"""
+def findMaxProduct(num, r):
+    maxProduct = 0
+    for i in range(len(num)-r):
+        product = 1
+        for j in range(i, i+r):
+            product = product * int(num[j])
+        
+        if product > maxProduct:
+            maxProduct = product
 
-groupLength = int(sys.argv[1])
-# Load file with a big number
-numberFile = open("number.txt", "r")
+    return maxProduct
 
-# Convert number to a string of digits
-numberString = ""
-for line in numberFile:
-    numberString += line
-numberString = numberString.replace("\n", "")
-
-maxProduct = 0
-for i in range(len(numberString)-groupLength):
-    product = 1
-    for j in range(i, i+groupLength):
-        product = product * int(numberString[j])
+if __name__ == '__main__':
+    # Load file with a big number
+    numberFile = open(sys.argv[1], "r")
+    # Convert number to a string of digits
+    numberString = ""
+    for line in numberFile:
+        numberString += line
+    numberString = numberString.replace("\n", "")
+    groupLength = int(sys.argv[2])
     
-    if product > maxProduct:
-        maxProduct = product
-
-print(maxProduct)
+    print(findMaxProduct(numberString, groupLength))
