9009e790e28113cd3c2466ecbaa5fa8c69663150
Alexios Karadimos <karadalex@gmail.com>
2018-11-26 23:15:25 +0200

    trying a second solution with less calculations (failing)

--- project-8/max_product.py
+++ project-8/max_product.py
@@ -16,7 +16,31 @@ def findMaxProduct1(num, r):
             maxProduct = product
 
     return maxProduct
-    
+
+"""
+Second solution
+num: digits in string form
+r:   lenght/range of each product group
+"""
+def findMaxProduct2(num, r):
+    maxProduct = 0
+    product = 1
+    # Assume that first digit is non-zero
+    for i in range(r):
+        product = product * int(num[i])
+
+    i = 1
+    while i < len(num)-r:
+        print(product)
+        product = int(product/int(num[i-1]))
+        product = product * int(num[i+r])
+        print(product)
+        if product > maxProduct:
+            maxProduct = product
+        
+        i += 1
+
+    return maxProduct
 
 if __name__ == '__main__':
     # Load file with a big number
